be92ab683f6e60618485878b460644bf7bb6a9c6
bocanegra <bocanegrajohan123@gmail.com>
2025-07-18 19:39:23 -0500

    Mostrando la Información - Vistas y Plantillas (MVT) Dia 2

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+# posts/views.py
+
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('title')
+    return render(request, 'posts/post_list.html', {'posts': posts})
